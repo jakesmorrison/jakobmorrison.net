281427623b0edcffaad013a8aee221eaa548b5ca
jakesmorrison <jakesmorrison4@gmail.com>
2017-04-06 21:30:20 -0600

    adding location

--- poo/models.py
+++ poo/models.py
@@ -4,10 +4,10 @@ from django.db import models
 
 class Poo(models.Model):
     name = models.CharField(max_length=100)
-    # loc = models.CharField(max_length=100, default="x")
+    loc = models.CharField(max_length=100, default="x")
     time = models.TimeField()
     date = models.DateField()
     movement = models.CharField(max_length=100)
     type = models.CharField(max_length=100)
     def __str__(self):
-        return "{} {} {} {} {}".format(self.name,self.time,self.date,self.movement,self.type)
+        return "{} {} {} {} {}".format(self.name,self.loc,self.time,self.date,self.movement,self.type)
