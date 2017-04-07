17eff61cdbbfe3b8332ef3a2b10edd5393f2a08e
jakesmorrison <jakesmorrison4@gmail.com>
2017-04-06 20:52:13 -0600

    new column

--- poo/models.py
+++ poo/models.py
@@ -4,9 +4,10 @@ from django.db import models
 
 class Poo(models.Model):
     name = models.CharField(max_length=100)
+    location = models.CharField(max_length=100, default="x")
     time = models.TimeField()
     date = models.DateField()
     movement = models.CharField(max_length=100)
     type = models.CharField(max_length=100)
     def __str__(self):
-        return "{} {} {} {} {}".format(self.name,self.time,self.date,self.movement,self.type)
+        return "{} {} {} {} {}".format(self.name,self.location,self.time,self.date,self.movement,self.type)
