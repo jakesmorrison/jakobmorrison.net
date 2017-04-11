0e1f59553de32f68d22cee06ac6906ff1dd6cdec
jakesmorrison <jakesmorrison4@gmail.com>
2017-04-10 23:07:27 -0600

    updating map

--- map/urls.py
+++ map/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 app_name = 'map'
 urlpatterns = [
-    url(r'', views.index, name='index'),
+    url(r'^$', views.index, name='index'),
+    url(r'^get_long_lat/', views.get_long_lat, name='get_long_lat'),
+    url(r'^send_long_lat/', views.send_long_lat, name='send_long_lat'),
 ]
