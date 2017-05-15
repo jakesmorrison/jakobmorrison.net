0399cb0893eaeca058adf7f52179776b731b86d7
jakesmorrison <jakesmorrison4@gmail.com>
2017-05-15 12:34:17 -0600

    adding asia travel app and softball stats.

--- softball/views.py
+++ softball/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import JsonResponse
 
 from .models import Stats
-from .models import Polls
+# from .models import Polls
 
 import pandas as pd
 import re
