8f7b78ebdbfdb4f005ccf18ec8e4667604394993
jakesmorrison <jakesmorrison4@gmail.com>
2017-06-22 01:54:30 -0600

    adding travel stats

--- travel/admin.py
+++ travel/admin.py
@@ -20,3 +20,10 @@ class IssuedModelAdmin(admin.ModelAdmin):
     search_fields = list_display
 from .models import Issued
 admin.site.register(Issued,IssuedModelAdmin)
+
+
+class TravelStatsModelAdmin(admin.ModelAdmin):
+    list_display = "self.date, self.city, self.country, self.traveling_with, self.wake_time, self.sleep_time, self.naps, self.breakfast_time, self.breakfast_cost, self.breakfast_type, self.lunch_time, self.lunch_cost, self.lunch_type, self.dinner_time, self.dinner_cost, self.dinner_type, self.glasses_of_wine, self.wine_cost, self.cocktail_count, self.cocktail_cost, self.miles_air, self.miles_train, self.miles_bus, self.miles_car, self.miles_walk, self.housing_cost, self.housing_name, self.housing_type, self.num_sex, self.num_master, self.num_poops, self.hours_read, self.book, self.show, self.hours_tv, self.num_pictures_taken, self.hours_podcast, self.podcast, self.hours_music, self.museums_visited, self.friends_met, self.tour_name, self.tour_cost".replace(" ", "").replace("self.","").split(",")
+    search_fields = list_display
+from .models import TravelStats
+admin.site.register(TravelStats,TravelStatsModelAdmin)
