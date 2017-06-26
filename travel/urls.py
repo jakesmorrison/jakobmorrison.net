08ad4dd124e246134ccb5dd173e935dd42594c07
jakesmorrison <jakesmorrison4@gmail.com>
2017-06-26 10:58:49 -0600

    adding travel stats

--- travel/urls.py
+++ travel/urls.py
@@ -4,4 +4,12 @@ from . import views
 app_name= 'travel'
 urlpatterns = [
     url(r'^$', views.home, name='home'),
+    # url(r'^euro', views.euro, name='euro'),
+    url(r'^general$', views.general, name='general'),
+    url(r'^sleep$', views.sleep, name='sleep'),
+    url(r'^media$', views.media, name='media'),
+    url(r'^transportation$', views.transportation, name='transportation'),
+    url(r'^other$', views.other, name='other'),
+    url(r'^food$', views.food, name='food'),
+
 ]
--- travel/views.py
+++ travel/views.py
@@ -1,10 +1,49 @@
 from django.shortcuts import render
-from .models import CreditCard, User, Issued
-
+from .models import TravelStats
+import pandas as pd
+from collections import Counter
 # Create your views here.
 
 def home(request):
     context = {
-
     }
-    return render(request, 'travel/home2.html', context)
+    return render(request, 'travel/stats_home.html', context)
+
+# def euro(request):
+#     context = {
+#     }
+#     return render(request, 'travel/stats_home.html', context)
+#
+
+def general(request):
+    df = pd.DataFrame(list(TravelStats.objects.all().values()))
+    city_day_counter = Counter(df["city"].tolist()).items()
+
+    city_list = [x[0] for x in city_day_counter]
+    city_day_count = [x[1] for x in city_day_counter]
+
+    context = {
+        "city_list": city_list,
+        "city_day_count": city_day_count,
+    }
+    return render(request, 'travel/general_info.html', context)
+
+def media(request):
+    context = {}
+    return render(request, 'travel/general_info.html', context)
+
+def sleep(request):
+    context = {}
+    return render(request, 'travel/general_info.html', context)
+
+def food(request):
+    context = {}
+    return render(request, 'travel/general_info.html', context)
+
+def transportation(request):
+    context = {}
+    return render(request, 'travel/general_info.html', context)
+
+def other(request):
+    context = {}
+    return render(request, 'travel/general_info.html', context)
