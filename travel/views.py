cba45a84d2718466ca563d4e6882f1b07933e206
jakesmorrison <jakesmorrison4@gmail.com>
2017-07-06 10:34:13 -0600

    adding table

--- travel/views.py
+++ travel/views.py
@@ -37,10 +37,28 @@ def sleep(request):
 
     df = pd.DataFrame(list(TravelStats.objects.all().values()))
 
+    df_city_wake_sleep = df[["city","wake_time","sleep_time"]]
+    df_city_wake_sleep["wake_time"] = df_city_wake_sleep["wake_time"].apply(lambda x: int(str(x).split(":")[1])+int(str(x).split(":")[1])/60)
+    df_city_wake_sleep["sleep_time"] = df_city_wake_sleep["sleep_time"].apply(lambda x: int(str(x).split(":")[1])+int(str(x).split(":")[1])/60)
+    df_city_wake_sleep = df_city_wake_sleep.groupby(['city']).mean().reset_index()
 
+    df_city_wake_sleep["wake_time"] = df_city_wake_sleep["wake_time"].apply(lambda x: str(str(x).split(".")[0]) + ":" +str((float(str(x).split(".")[1])*60)/1000).replace(".",""))
+    df_city_wake_sleep["sleep_time"] = df_city_wake_sleep["sleep_time"].apply(lambda x: str(str(x).split(".")[0]) + ":" +str((float(str(x).split(".")[1])*60)/1000).replace(".",""))
+
+    city = list(df_city_wake_sleep["city"].tolist())
+    wake = list(df_city_wake_sleep["wake_time"].tolist())
+    sleep = list(df_city_wake_sleep["sleep_time"].tolist())
+
+    city_wake_sleep = []
+    for n,x in enumerate(city):
+        city_wake_sleep.append({'city': city[n],'wake':wake[n], 'sleep':sleep[n]})
+
+    # For radial charts
     df_wake_time = df["wake_time"].tolist()
+    df_sleep_time = df["sleep_time"].tolist()
     wake_list = []
-    for x in df_wake_time:
+    sleep_list = []
+    for n,x in enumerate(df_wake_time):
         wake_time = (str(x).split(":"))
         if int(wake_time[0]) == 23 and int(wake_time[1])>=30:
             wake_time = 0
@@ -50,15 +68,7 @@ def sleep(request):
             wake_time = int(wake_time[0])
         wake_list.append(wake_time)
 
-    wake_dict = Counter(wake_list)
-    radial_wake_list = []
-    for key,val in wake_dict.items():
-        radial_wake_list.append({'type':'line','name':'Occurrences','data':[0,val],'pointStart':0,'pointInterval':key,'color':'black','marker':{'symbol':'circle'}})
-
-    df_sleep_time = df["sleep_time"].tolist()
-    sleep_list = []
-    for x in df_sleep_time:
-        sleep_time = (str(x).split(":"))
+        sleep_time = (str(df_sleep_time[n]).split(":"))
         if int(sleep_time[0]) == 23 and int(sleep_time[1])>=30:
             sleep_time = 0
         elif int(sleep_time[1])>=30:
@@ -67,12 +77,19 @@ def sleep(request):
             sleep_time = int(sleep_time[0])
         sleep_list.append(sleep_time)
 
+
+    wake_dict = Counter(wake_list)
+    radial_wake_list = []
+    for key,val in wake_dict.items():
+        radial_wake_list.append({'type':'line','name':'Occurrences','data':[0,val],'pointStart':0,'pointInterval':key,'color':'black','marker':{'symbol':'circle'}})
+
     sleep_dict = Counter(sleep_list)
     radial_sleep_list = []
     for key,val in sleep_dict.items():
         radial_sleep_list.append({'type':'line','name':'Occurrences','data':[0,val],'pointStart':0,'pointInterval':key,'color':'black','marker':{'symbol':'circle'}})
 
     context = {
+        'city_wake_sleep': city_wake_sleep,
         'radial_wake': radial_wake_list,
         'radial_sleep': radial_sleep_list,
     }
