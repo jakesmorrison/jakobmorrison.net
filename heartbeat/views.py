f689650087953a3a3652ae2120b56249879d1c43
jakesmorrison <jakesmorrison4@gmail.com>
2017-09-24 14:05:26 -0600

    adding investment checkup.

--- heartbeat/views.py
+++ heartbeat/views.py
@@ -4,4 +4,4 @@ from django.shortcuts import render
 def index(request):
     context={
     }
-    return render(request,"heartbeat/index2.html",context)
+    return render(request,"heartbeat/index3.html",context)
