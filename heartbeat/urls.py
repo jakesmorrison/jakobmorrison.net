f586414d76ab3b3eea72da3a9b13074f7aeddaef
jakesmorrison <jakesmorrison4@gmail.com>
2017-09-25 19:28:47 -0600

    adding investment checkup.

--- heartbeat/urls.py
+++ heartbeat/urls.py
@@ -4,4 +4,7 @@ from . import views
 app_name = 'heartbeat'
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^investment_analyzer$', views.investment_analyzer, name='investment_analyzer'),
+    url(r'^withdraw_analyzer$', views.withdraw_analyzer, name='withdraw_analyzer'),
+
 ]
--- heartbeat/views.py
+++ heartbeat/views.py
@@ -4,4 +4,15 @@ from django.shortcuts import render
 def index(request):
     context={
     }
+    return render(request,"heartbeat/index2.html",context)
+
+def investment_analyzer(request):
+    context={
+    }
     return render(request,"heartbeat/index3.html",context)
+
+
+def withdraw_analyzer(request):
+    context={
+    }
+    return render(request,"heartbeat/index4.html",context)
